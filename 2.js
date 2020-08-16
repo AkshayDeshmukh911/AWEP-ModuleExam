083d648b60479f95e58f6ed3049760ac1f7d41c3
Akshay Deshmukh <65339947+AkshayDeshmukh911@users.noreply.github.com>
2020-08-16 12:07:01 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,25 @@
+function submithere(){
+    let x1=document.queryselector("#id1").value;
+      let y1= document.querySelector("#submitIt").cloneNode;
+    if(x1==="")
+    {
+        y1.innerhtml=x1;
+      y1.document.insertbefore(x1,y1.firstchild);
+    }
+    else{
+    
+    }
+
+    
+    let x2=document.queryselector("#id2").value;
+    let y2=let y1= document.querySelector("#submitIt").cloneNode;
+    if(x2 > 1 || x2 < 8)
+    {
+        y2.innerhtml=x2;
+      y2.document.insertbefore(x2,y2.firstchild);
+    }
+    ifelse(x2<1 || x2>8){
+          y2.innerhtml=alert("password maximum 8 character only")
+    }
+
+}
